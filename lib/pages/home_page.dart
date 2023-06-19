8a6d4c75a9f14e59bcae5ebe6941c98aa980160c
framadhan99 <fajarramadhan99704@gmail.com>
2023-06-19 10:03:16 +0700

    detail page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,6 +3,8 @@ import 'package:password_manager_flutter/config/asset_colors.dart';
 import 'package:password_manager_flutter/config/asset_styles.dart';
 import 'package:password_manager_flutter/config/ui_helper.dart';
 import 'package:password_manager_flutter/pages/from_page.dart';
+import 'package:password_manager_flutter/widgets/button_primary.dart';
+import 'package:password_manager_flutter/widgets/textfield_general.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -10,55 +12,134 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'Dashboard',
-            style: AssetStyles.bigTitle.copyWith(color: AssetColors.greyDark),
+      body: Center(
+        child: Container(
+          margin: EdgeInsets.all(50),
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          width: 400,
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: AssetColors.greenLight,
+              width: 1,
+            ),
+            borderRadius: BorderRadius.circular(16),
+            color: Colors.white,
           ),
-          verticalSpace(20),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+          child: Column(
             children: [
-              ...List.generate(3, (index) {
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    height: 200,
-                    width: 200,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: AssetColors.greenDark,
+              verticalSpace(40),
+              Row(
+                children: [
+                  SizedBox(
+                    width: 33,
+                  ),
+                  Spacer(
+                    flex: 2,
+                  ),
+                  Text(
+                    'Categories',
+                    style: AssetStyles.mediumTitle,
+                  ),
+                  Spacer(
+                    flex: 2,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return Dialog(
+                            child: Container(
+                                padding: EdgeInsets.all(30),
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Text('Category'),
+                                    verticalSpace(5),
+                                    SizedBox(
+                                      width: 200,
+                                      child: TextFieldGeneral(),
+                                    ),
+                                    verticalSpace(20),
+                                    ButtonPrimary(
+                                      width: 100,
+                                      label: 'Add',
+                                    )
+                                  ],
+                                )),
+                          );
+                        },
+                      );
+                    },
+                    child: Container(
+                      height: 23,
+                      width: 33,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(4),
+                        color: AssetColors.orangePastel,
+                      ),
+                      child: const Icon(
+                        Icons.add,
+                        color: Colors.white,
+                        size: 18,
+                      ),
                     ),
-                    alignment: Alignment.center,
-                    child: Column(
+                  ),
+                  // Spacer(),
+                ],
+              ),
+              verticalSpace(30),
+              ...List.generate(4, (index) {
+                return Padding(
+                  padding: const EdgeInsets.only(bottom: 15),
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => FormPage(),
+                          ));
+                    },
+                    child: Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         Text(
                           'Categories',
-                          style: AssetStyles.mediumTitle
-                              .copyWith(color: Colors.white),
-                        ),
-                        verticalSpace(20),
-                        Text(
-                          '5',
                           style: AssetStyles.mediumTitle.copyWith(
-                            color: Colors.white,
-                            fontSize: 50,
+                            fontSize: 16,
                           ),
                         ),
+                        Spacer(),
+                        Container(
+                          height: 28,
+                          width: 28,
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: AssetColors.orangePastel,
+                          ),
+                          alignment: Alignment.center,
+                          child: Text(
+                            '5',
+                            style: AssetStyles.bigDescription.copyWith(
+                              color: Colors.white,
+                              fontSize: 14,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ),
+                        Icon(Icons.delete),
+                        Icon(Icons.edit)
                       ],
                     ),
                   ),
                 );
-              })
+              }),
+              verticalSpace(50),
+              ButtonPrimary(label: 'Save')
             ],
           ),
-        ],
+        ),
       ),
-    ));
+    );
   }
 }
